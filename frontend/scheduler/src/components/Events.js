3e5cd5891b9e594cfab3736bbaff179e28d07c6c
davidalveno <davidralveno@gmail.com>
2018-08-10 15:11:09 -0600

    Refactoring: chaning tab size from 4 to 2 spaces

--- frontend/scheduler/src/components/Events.js
+++ frontend/scheduler/src/components/Events.js
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types';
 import '../css/LoginForm.css';
 
 class Events extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
     }
+  }
 
-    render () {
-        return (
-            <div className="Events">
-                This is the Events!!!
+  render() {
+    return (
+      <div className="Events">
+        This is the Events!!!
             </div>
-        )
-    }
+    )
+  }
 }
 
 export default Events;
--- frontend/scheduler/src/components/Stuff.js
+++ frontend/scheduler/src/components/Stuff.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 
 class Loader extends Component {
 
-    componentDidMount() {
-        window.componentHandler.upgradeDom();
-    }
+  componentDidMount() {
+    window.componentHandler.upgradeDom();
+  }
 
-    componentDidUpdate() {
-        window.componentHandler.upgradeDom();
-    }
-    
-    render() {
-        return (
-            // MDL Spinner Component
-            <div className="mdl-spinner mdl-js-spinner is-active"></div>
-        )
-    }
+  componentDidUpdate() {
+    window.componentHandler.upgradeDom();
+  }
+
+  render() {
+    return (
+      // MDL Spinner Component
+      <div className="mdl-spinner mdl-js-spinner is-active"></div>
+    )
+  }
 }
 
 export default Loader;
